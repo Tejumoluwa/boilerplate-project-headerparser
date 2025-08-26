16af62d6465f6a6b38fae1e544b337a8ecb48150
Teju <tejumoluwaogunleye@gmail.com>
2025-08-26 18:54:46 +0100

    Completed challenge

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const responseInfo = {
+    ipaddress: req.ip, 
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  }
+  res.json(responseInfo);
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
